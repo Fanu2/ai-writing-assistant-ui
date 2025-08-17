32f3809dc86269931ba310d46c0a5717bc15819b
JasCoder <85804831+Fanu2@users.noreply.github.com>
2025-08-17 15:35:25 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
-module.exports={reactStrictMode:true}
+module.exports={reactStrictMode:true}/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
